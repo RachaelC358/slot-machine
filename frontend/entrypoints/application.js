0e9d048809733e3cfb86eb4a849c4f16b1e4c3fc
Rachael Carpenter <carprl@outlook.com>
2025-02-25 21:58:53 -0600

    add react skeleton to main.tsx

--- frontend/entrypoints/application.js
+++ frontend/entrypoints/application.js
@@ -3,6 +3,5 @@
 // When using a plain API, perhaps it's better to generate an HTML entrypoint
 // and link to the scripts and stylesheets, and let Vite transform it.
 console.log('Vite ⚡️ Ruby')
-
 // Example: Import a stylesheet in <sourceCodeDir>/index.css
 // import '~/index.css'
