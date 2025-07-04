983db89fd21ba5fcff541a32552c90274b18a4c4
Rachael Carpenter <carprl@outlook.com>
2025-07-03 22:10:57 -0500

    implement picked color as ccs var, save on home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,25 +3,36 @@ import { useState, useEffect } from 'react';
 import Counter from '../components/Counter';
 import ColorPicker from 'react-pick-color';
 
-
 export default function HomePage() {
   const [displayCount, setDisplayCount] = useState(0);
+  const [color, setColor] = useState('#646cff'); 
 
   useEffect(() => {
-    
+    const storedColor = localStorage.getItem('userColor');
+    if (storedColor) {
+      setColor(storedColor);
+      applyColorScheme(storedColor);
+    }
+
     const initialCount = 100; 
     setDisplayCount(initialCount);
   }, []);
 
-  const [color, setColor] = useState('#fff');
+  function applyColorScheme(newColor: string) {
+    document.documentElement.style.setProperty('--primary-color', newColor);
+  }
 
+  function handleColorChange(newHex: string) {
+    setColor(newHex);
+    localStorage.setItem('userColor', newHex);
+    applyColorScheme(newHex);
+  }
 
   return (
     <div>
       <h1>Home</h1>
       <Counter count={displayCount} />
-
-      <ColorPicker color={color} onChange={color => setColor(color.hex)} />;
+      <ColorPicker color={color} onChange={(color) => handleColorChange(color.hex)} />
     </div>
   );
 }
